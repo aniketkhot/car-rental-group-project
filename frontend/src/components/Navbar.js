9c7fb97470f801ef3f13c61b627c15fd004465a5
Sio Kong Si <siokong.si@connect.qut.edu.au>
2025-05-18 20:35:03 +1000

    modified about the frontend navbar format and connect the view button of the serach page and the affter search to the car detail pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,106 +19,106 @@ function Navbar() {
     fontSize: '16px', 
     color: '#333',    };
 
-  return (
-    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#78B3CE' }}>
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          <img src="./renteal_logo.png" alt="Car Rental System" style={{ height: '40px' }} />
-        </NavLink>
-        <div className="d-flex ms-auto">
-          <ul className="navbar-nav">
-            {user &&  (
-              <>
-                <li className="nav-item">
-                  <span className="nav-link" style={commonStyle}>Welcome, {user.name}</span>
+    return (
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#78B3CE' }}>
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img src="./renteal_logo.png" alt="Car Rental System" style={{ height: '40px' }} />
+          </NavLink>
+          <div className="d-flex ms-auto">
+            <ul className="navbar-nav">
+              {user &&  (
+                <>
+                 <li className="nav-item">
+                  <span className="nav-link" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>Welcome, {user.name}</span>
                 </li>
-    
-                {user && user.role !== 'admin' && (
-                          <>
-                           
-                            <li className="nav-item">
-                              <NavLink className="nav-link" to="/customer-dashboard" style={commonStyle}>
-                                customer-dashboard
-                              </NavLink>
-                            </li>
-                             <li className="nav-item">
-                  <NavLink className="nav-link" to="/searchpage" style={commonStyle}>
-                    searchpage
-                  </NavLink>
-                   </li>
-                    <li className="nav-item">
-                  <NavLink className="nav-link" to="/checkout" style={commonStyle}>
-                    checkout
-                  </NavLink>
-                   </li>
-                 
-               
-                          </>
-                          
-                        )}
-                {isAdmin && (
-                  <>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/cars" style={commonStyle}>
-                        Cars
-                      </NavLink>
+      
+                  {user && user.role !== 'admin' && (
+                            <>
+                            
+                              <li className="nav-item">
+                                <NavLink className="nav-link" to="/customer-dashboard" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                                  customer-dashboard
+                                </NavLink>
+                              </li>
+                              <li className="nav-item">
+                    <NavLink className="nav-link" to="/searchpage" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                      searchpage
+                    </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/customers" style={commonStyle}>
-                        Customers
-                      </NavLink>
+                      <li className="nav-item">
+                    <NavLink className="nav-link" to="/checkout" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                      checkout
+                    </NavLink>
                     </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/rentals" style={commonStyle}>
-                        Rentals
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-              </>
-            )}
-
-            {!user ? (
-              <>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/login" style={commonStyle}>
-                    Login
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/register" style={commonStyle}>
-                    Register
-                  </NavLink>
-                 
-                   </li>
-                            <li className="nav-item">
-                  <NavLink className="nav-link" to="/searchpage" style={commonStyle}>
-                    searchpage
-                  </NavLink>
-                   </li>
-                    <li className="nav-item">
-                  <NavLink className="nav-link" to="/checkout" style={commonStyle}>
-                    checkout
-                  </NavLink>
-                   </li>
                   
-              </>
-            ) : (
-              <li className="nav-item">
-                <button
-                  className="btn btn-link nav-link"
-                  onClick={handleLogout}
-                  style={commonStyle}
-                >
-                  Logout
-                </button>
-              </li>
-            )}
-          </ul>
+                
+                            </>
+                            
+                          )}
+                  {isAdmin && (
+                    <>
+                      <li className="nav-item">
+                        <NavLink className="nav-link" to="/cars" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                          Cars
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink className="nav-link" to="/customers" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                          Customers
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink className="nav-link" to="/rentals" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                          Rentals
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
+                </>
+              )}
+
+              {!user ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                      Login
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/register" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                      Register
+                    </NavLink>
+                  
+                    </li>
+                              <li className="nav-item">
+                    <NavLink className="nav-link" to="/searchpage" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                      searchpage
+                    </NavLink>
+                    </li>
+                      <li className="nav-item">
+                    <NavLink className="nav-link" to="/checkout" style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}>
+                      checkout
+                    </NavLink>
+                    </li>
+                    
+                </>
+              ) : (
+                <li className="nav-item">
+                  <button
+                    className="btn btn-link nav-link"
+                    onClick={handleLogout}
+                    style={{ fontWeight: 'bold',fontFamily: 'Roboto slab, sans-serif' }}
+                  >
+                    Logout
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-}
+      </nav>
+    );
+  }
 
 export default Navbar;
